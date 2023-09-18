b98c714e742c3d2dc6b827b88cdffd452e300244
cecgil <dudigil6@gmail.com>
2023-09-17 23:25:15 -0300

    feat: My first feature

---
+++ server/summarize.js
@@ -0,0 +1,16 @@
+import { pipeline } from '@xenova/transformers'
+import {summaryExample} from './utils/summary.js'
+
+export async function summarize(text) {
+  try {
+
+    const generator = await pipeline("summarization",
+    "Xenova/distilbart-cnn-12-6")
+
+    const output = await generator(text)
+    return output[0].summary_text
+  } catch (error) {
+    throw new Error (error)
+  }
+  
+}
